03744bfce241489c3442d022ea7cb0514b0a2dc0
chan-00 <a82316@naver.com>
2023-02-04 15:56:12 +0900

    팀 생성 시 입력 제한 규칙 추가

--- src/Functions/FunctionTeam/functionTeamMake.js
+++ src/Functions/FunctionTeam/functionTeamMake.js
@@ -2,6 +2,26 @@ import axios from "axios";
 import server_ip from "../../serverIP";
 
 function functionTeamMake(idValue, teamnameRef, teamDescRef, teamCategory, navigate) {
+    //백엔드와 통신하기 전 입력한 내용의 길이를 체크하는 조건문 코드이다.
+    if(teamnameRef.current.value.length === 0 || teamnameRef.current.value.length > 100) {
+        alert("팀 이름을 입력하지 않았거나 100글자 이상입니다.");
+        teamnameRef.current.focus();
+        return false;
+    }
+    else if(teamDescRef.current.value.length > 400) {
+        alert("팀 설명이 400글자 이상입니다.");
+        teamDescRef.current.focus();
+        return false;
+    }
+
+    //팀명을 정규 표현식으로 입력에 제한을 거는 코드이다.
+    const teamnamePattern = /^[a-zA-Zㄱ-힣][a-zA-Zㄱ-힣]*$/;
+    if(!teamnamePattern.test(teamnameRef.current.value)) {
+        alert("현재 입력한 팀 이름에 특수문자 혹은 공백이 포함되어 있습니다.");
+        teamnameRef.current.focus();
+        return false;
+    }
+
     axios.post("http://" + server_ip + ":8000/back/make_team/", {
         id: idValue,
         teamname: teamnameRef.current.value,
