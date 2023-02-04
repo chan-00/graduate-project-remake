c0c5715141dea1e041e1873835cc527ba7d02556
chan-00 <a82316@naver.com>
2023-02-04 16:44:17 +0900

    vercel 적용을 위한 gitignore 변경

---
+++ src/searchApiKey.js
@@ -0,0 +1,6 @@
+const searchApi = {
+    apiKey: "AIzaSyDrFyJN1-fROhswKxg9uR-drI2M5efygvg",
+    cxValue: "12f8da65ee3854199"
+};
+
+export default searchApi;
---
+++ src/serverIP.js
@@ -0,0 +1,3 @@
+const server_ip = "14.56.112.146";
+
+export default server_ip;
