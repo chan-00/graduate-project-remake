08c33d0385714f02458ecd80b957ce64e53baceb
chan-00 <a82316@naver.com>
2023-02-07 12:38:15 +0900

    vercel 적용 해제를 위한 코드 변경

--- src/Functions/FunctionSign/functionSignIn.js
+++ src/Functions/FunctionSign/functionSignIn.js
@@ -3,7 +3,7 @@ import server_url from "../../serverUrl.js";
 
 //로그인 이벤트 발생 시 호출되는 함수로, 로그인 창에 입력한 id와 pw 값을 갖고 DB에 select 문을 요청하는 로그인 함수이다.
 function functionSignIn(idRef, pwRef, navigate, setNickname) {
-    console.log(process.env.server_ip);
+    
     axios.post(server_url + "/back/login/", {
         id: idRef.current.value,
         pw: pwRef.current.value,
